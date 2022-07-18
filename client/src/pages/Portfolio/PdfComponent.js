44f8f680d90c9f733a346936a791678455ca9b32
ssryoung <qkrtpfud816@gmail.com>
2022-07-19 03:31:00 +0900

    [RFT] button style 수정

--- client/src/pages/Portfolio/PdfComponent.js
+++ client/src/pages/Portfolio/PdfComponent.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactToPrint from 'react-to-print';
+import styled from 'styled-components';
 
 import Portfolio from './Portfolio';
- 
+
 class PdfComponent extends React.Component {
-     
-    render() {
-      return (
-        <div>
-          <ReactToPrint
-            content={() => this.componentRef}
-            trigger={() => <button className="pdfbtn btn btn-primary">PDF로 저장</button>}
-          />
-          <Portfolio ref={(response) => (this.componentRef = response)} />
-        </div>
-      );
-    }
- 
+  render() {
+    return (
+      <div>
+        <ReactToPrint
+          content={() => this.componentRef}
+          trigger={() => (
+            <button className="btn btn-primary" style={styles.button}>
+              PDF로 저장
+            </button>
+          )}
+        />
+        <Portfolio ref={(response) => (this.componentRef = response)} />
+      </div>
+    );
+  }
 }
+
+const styles = { button: { position: 'fixed', bottom: '30px', right: '30px' } };
 
 export default PdfComponent;
