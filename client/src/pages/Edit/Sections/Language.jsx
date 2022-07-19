a73b7f11f4619567d6e46fff04bc18b74afef244
ssryoung <qkrtpfud816@gmail.com>
2022-07-20 02:04:42 +0900

    [ADD] section - 외국어 등록 api 연동

--- client/src/pages/Edit/Sections/Language.jsx
+++ client/src/pages/Edit/Sections/Language.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useRef } from 'react';
 import LanguageList from './LanguageList';
+import axios from 'axios';
 
 function Language() {
   const [inputs, setInputs] = useState({
@@ -31,14 +32,40 @@ function Language() {
       languageName,
       languageScore,
     };
-    setLanguages(languages.concat(language));
+    if (languageDate === '') {
+      alert('취득일을 등록해주세요');
+    } else if (languageName === '') {
+      alert('언어 명을 등록해주세요');
+    } else if (languageScore === '') {
+      alert('점수/등급을 등록해주세요');
+    } else if (
+      languageDate !== '' &&
+      languageName !== '' &&
+      languageScore !== ''
+    ) {
+      setLanguages(languages.concat(language));
 
-    setInputs({
-      languageDate: '',
-      languageName: '',
-      languageScore: '',
-    });
-    nextId.current += 1;
+      const data = {
+        name: languageName,
+        level: languageScore,
+        acquisition_date: languageDate,
+        portfolio_idx: 1,
+      };
+
+      if (languageName && languageScore && languageDate) {
+        axios
+          .post('http://localhost:3001/languages', data)
+          .then((res) => console.log(res, '성공'))
+          .catch((err) => console.log(err, '실패'));
+      }
+
+      setInputs({
+        languageDate: '',
+        languageName: '',
+        languageScore: '',
+      });
+      nextId.current += 1;
+    }
   };
 
   // 삭제
