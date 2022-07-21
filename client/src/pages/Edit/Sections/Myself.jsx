706d123a923502a6bcfc77cdc27e470e2528cdc9
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-07-21 09:51:57 +0900

    [ADD]한 줄 소개 api 요청시 필요한 정보 갱신

--- client/src/pages/Edit/Sections/Myself.jsx
+++ client/src/pages/Edit/Sections/Myself.jsx
@@ -13,31 +13,47 @@ function Myself() {
   const [mySelf, setMySelf] = useState('');
   const [newMySelf, setNewMySelf] = useState();
 
+  const { portfolio_idx } = useParams();
+
   function mySelfChange(e) {
     setMySelf(e.target.value);
   }
 
   // 한 줄 소개 GET
   const getStack = async () => {
-    await axios.get(`http://localhost:3001/portfolios/1`).then((res) => {
-      setNewMySelf(res.data.data.introduce[0].comment);
-    });
+    await axios
+      .get(`http://localhost:3001/portfolios/${portfolio_idx}`)
+      .then((res) => {
+        // console.log(res.data.data.introduce[0].introduce_idx);
+        setNewMySelf(res.data.data.introduce);
+      });
   };
   useEffect(() => {
     getStack();
   }, []);
 
-  // 한 줄 소개 POST
+  // 한 줄 소개 POST, PATCH
   async function postIntroduce(data) {
     const newData = {
       comment: data.comment,
-      portfolio_idx: 1,
+      portfolio_idx: portfolio_idx,
     };
-    setNewMySelf(data.comment);
-    // await axios
-    //   .post('http://localhost:3001/introduces', newData)
-    //   .then((res) => console.log(res))
-    //   .catch((err) => console.log(err));
+    if (newMySelf.length >= 1) {
+      console.log('업데이트');
+      await axios
+        .patch(
+          `http://localhost:3001/introduces/${newMySelf[0].introduce_idx}`,
+          newData,
+        )
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    } else {
+      console.log('새로운 생성');
+      await axios
+        .post('http://localhost:3001/introduces', newData)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
   }
 
   // 한 줄 소개 delete
@@ -46,6 +62,18 @@ function Myself() {
       .delete(`http://localhost:3001/introduces/${delIdx.introduces_idx}`)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
+  }
+
+  //테스트 함수
+  async function testz() {
+    if (newMySelf.length < 1) {
+      console.log('asdf');
+    }
+    console.log(newMySelf);
+    console.log(newMySelf[0].introduce_idx);
+    // await axios.delete(`http://localhost:3001/introduces/8`).then((res) => {
+    //   console.log(res);
+    // });
   }
 
   return (
@@ -58,7 +86,10 @@ function Myself() {
         ></textarea>
         {errors.comment && <div>{errors.comment.message}</div>}
         <button type="submit">등록</button>
-        {newMySelf ? <div>한 줄 소개 : {newMySelf}</div> : null}
+        <button type="button" onClick={testz}>
+          test 버튼
+        </button>
+        {newMySelf ? <div>한 줄 소개 : {newMySelf[0].comment}</div> : null}
       </form>
     </div>
   );
